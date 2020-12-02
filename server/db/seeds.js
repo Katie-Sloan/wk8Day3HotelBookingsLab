f803c160e62e680be03743eb3c1865c7dd269663
Chris Kite <chris.kat82@googlemail.com>
2020-12-02 12:47:43 +0000

    initial commit - hotel_bookings

---
+++ server/db/seeds.js
@@ -0,0 +1,22 @@
+use hotel;
+db.dropDatabase();
+
+db.bookings.insertMany([
+    {
+        name: "Chris Kite",
+        email: "chrisk@gmail.com",
+        checkedIn: false
+    },
+    {
+        name: "Katie Sloan",
+        email: "katies@gmail.com",
+        checkedIn: true
+    },
+    {
+        name: "John Harper",
+        email: "johnh@gmail.com",
+        checkedIn: true
+    }
+
+    
+]);
